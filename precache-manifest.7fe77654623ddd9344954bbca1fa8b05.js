a217640c07c53220aac4536509ce1c590dc4e164
Tim Paine <t.paine154@gmail.com>
2020-12-17 19:16:58 -0500

    Updates

---
+++ precache-manifest.7fe77654623ddd9344954bbca1fa8b05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2585ab8e68ab277327a8559897bdef9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0327400e9f62e5ed0154",
+    "url": "/static/css/main.98fb3426.chunk.css"
+  },
+  {
+    "revision": "142764c87866e2fdb53f",
+    "url": "/static/js/2.7c9e6dfc.chunk.js"
+  },
+  {
+    "revision": "0327400e9f62e5ed0154",
+    "url": "/static/js/main.10ca5f2c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
